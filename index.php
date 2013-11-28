3f6ae041ee46197508128baee95dd1e98d2e1b5b
Benoit Duffez <benoit@bicou.net>
2013-11-28 14:32:50 +0100

    updated main script

--- index.php
+++ index.php
@@ -1,4 +1,16 @@
+<style>
+div.error {
+	background: #FAA;
+	border: 1px dashed red;
+	padding: 10px;
+	margin: 10px;
+}
+div.warning {
+	border: 1px dashed #CCC;
+}
+</style>
 <?php
+
 error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
 ini_set('display_errors', '1');
 
@@ -19,7 +31,7 @@ include "Config.php";
 include "Context.php";
 
 try {
-	$pdo = new PDO("mysql:host=$_db_host;dbname=$_db_base", $_db_user, $_db_pass);
+	$pdo = new PDO("mysql:host=$_db_host;dbname=$_db_name", $_db_user, $_db_pass);
 	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
 	L("Unable to connect to DB server: $e");
@@ -27,15 +39,7 @@ try {
 }
 
 $cfg = new Config($pdo);
-$db_version = 0 + $cfg->get(Config::DB_VERSION);
-
-$ctx = new ContextDbAdapter($pdo, $db_version);
-if ($db_version == 0) {
-	$cfg->install();
-	$ctx->install();
-	$db_version = 0 + $cfg->get(Config::DB_VERSION);
-}
-
+$ctx = new ContextDbAdapter($pdo);
 $contexts = $ctx->loadAll();
 var_dump($contexts);
 
