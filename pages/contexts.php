37d6fe59bf3251341605d0e0fa01bf4360501180
Benoit Duffez <benoit@bicou.net>
2013-12-01 00:41:13 +0100

    ability to link string with context

--- pages/contexts.php
+++ pages/contexts.php
@@ -10,9 +10,40 @@ if (isset($_POST['context_name'])) {
 	echo "<p>Context added.</p>";
 } else {
 	echo '
-<form method="post" action="' . $_SERVER['REQUEST_URI'] . '">
+<form id="context" method="post" action="' . $_SERVER['REQUEST_URI'] . '">
 Context name: <input type="text" name="context_name" />
 <input type="submit" value="Add context"/>
 </form>';
 }
+
+include "db/Links.php";
+$links = new LinksDbAdapter();
+$sdb = new StringsDbAdapter();
+
+if (isset($_POST['context_id']) && isset($_POST['strings']) && is_array($_POST['strings'])) {
+	$contextId = 0 + $_POST['context_id'];
+	foreach ($_POST['strings'] as $stringName => $selected) {
+		if ($selected) {
+			$links->addLink(DbAdapter::TABLE_CONTEXTS, $_POST['context_id'], DbAdapter::TABLE_STRINGS, $stringName);
+		}
+	}
+	echo "<p>String" . (count($_POST['strings']) == 1 ? "" : "s") . " linked to context.</p>";
+} else {
+	echo '
+<form id="link" method="post" action="' . $_SERVER['REQUEST_URI'] . '">
+Context: <select name="context_id">';
+	foreach ($contexts as $context) {
+		echo '<option value="' . $context->id . '">' . $context->name . '</option>';
+	}
+	echo '
+</select>
+Strings: ';
+	foreach ($sdb->getAll($cfg->getDefaultLanguage()) as $context) {
+		echo '<input type="checkbox" name="strings[' . $context['name'] . ']" value="true" id="cb_' . $context['name'] . '"><label for="cb_' . $context['name'] . '">' . $context['name'] . '</label>';
+	}
+}
+echo '
+</select>
+<input type="submit" value="Link String with context"/>
+</form>';
 
