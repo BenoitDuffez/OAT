3983abd3a7fe3898db528b600751f79c8da48752
Benoit Duffez <benoit@bicou.net>
2013-11-30 23:56:12 +0100

    slight modifications of links table

--- db/Links.php
+++ db/Links.php
@@ -8,17 +8,21 @@
 class LinksDbAdapter extends DbAdapter {
 	const DB_VERSION = 1;
 
+	public function __construct(){
+		parent::__construct(DbAdapter::TABLE_LINKS, LinksDbAdapter::DB_VERSION);
+	}
+
 	protected function onUpgrade($oldVersion, $newVersion) {
 		if ($oldVersion < 1) {
 			$sql = <<<SQL
 CREATE TABLE IF NOT EXISTS table (
   id int(11) NOT NULL AUTO_INCREMENT,
-  table1 varchar(50) NOT NULL,
-  table2 varchar(50) NOT NULL,
-  id1 int(11) NOT NULL,
-  id2 int(11) NOT NULL,
+  tbl1 varchar(50) NOT NULL,
+  id1 varchar(100) NOT NULL,
+  tbl2 varchar(50) NOT NULL,
+  id2 varchar(100) NOT NULL,
   PRIMARY KEY (id),
-  UNIQUE KEY link (table1, table2, id1, id2)
+  UNIQUE KEY link (table1, id1, table2, id2)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 SQL;
 			$this->createTable($sql);
@@ -27,7 +31,7 @@ SQL;
 
 	public function addLink($table1, $id1, $table2, $id2) {
 		try {
-			$sql = "INSERT INTO " . DbAdapter::getTable(DbAdapter::TABLE_LINKS) . " (table1, table2, id1, id2) VALUES (?, ?, ?, ?)";
+			$sql = "INSERT INTO " . DbAdapter::getTable(DbAdapter::TABLE_LINKS) . " (tbl1, tbl2, id1, id2) VALUES (?, ?, ?, ?)";
 			$handle = $this->pdo->prepare($sql);
 			$i = 1;
 			$handle->bindValue($i++, $table1);
