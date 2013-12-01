fac302b1e6d3ac06a30c74088cdecd7ab78e0228
Benoit Duffez <benoit@bicou.net>
2013-12-01 13:28:36 +0100

    proper error handling for db connection

--- index.php
+++ index.php
@@ -89,10 +89,10 @@ include "db/Screenshots.php";
 
 // Init DB connection
 try {
-	$pdo = new PDO("mysql:host=$_db_host;dbname=$_db_name", $_db_user, $_db_pass);
+	$pdo = @new PDO("mysql:host=$_db_host;dbname=$_db_name", $_db_user, $_db_pass);
 	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-	L("Unable to connect to DB server: $e");
+	L("Unable to connect to DB server:", $e);
 	die();
 }
 
