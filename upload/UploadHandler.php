1a6499bacaaeb1359c26fdd418b5deb8bcea9d4f
Benoit Duffez <benoit@bicou.net>
2013-12-02 19:23:52 +0100

    always rename files to current timestamp bigger uploaded image thumbnails

--- upload/UploadHandler.php
+++ upload/UploadHandler.php
@@ -140,8 +140,8 @@ class UploadHandler
                     // Uncomment the following to force the max
                     // dimensions and e.g. create square thumbnails:
                     //'crop' => true,
-                    'max_width' => 80,
-                    'max_height' => 80
+                    'max_width' => 250,
+                    'max_height' => 250
                 )
             )
         );
@@ -450,9 +450,9 @@ class UploadHandler
         // Also remove control characters and spaces (\x00..\x20) around the filename:
         $name = trim(basename(stripslashes($name)), ".\x00..\x20");
         // Use a timestamp for empty filenames:
-        if (!$name) {
+//        if (!$name) {
             $name = str_replace('.', '-', microtime(true));
-        }
+//        }
         // Add missing file extension for known image types:
         if (strpos($name, '.') === false &&
             preg_match('/^image\/(gif|jpe?g|png)/', $type, $matches)) {
