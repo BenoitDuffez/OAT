e198666b96134a1a1bf363fb15c0ad0c34105b2f
Benoit Duffez <benoit@bicou.net>
2013-12-03 00:53:59 +0100

    moved initialization code into a separate php script

--- index.php
+++ index.php
@@ -29,19 +29,7 @@ ini_set('display_errors', '1');
 
 readfile("static/header.html");
 
-// Main includes
-include "config.db.php";
-include "lang.php";
-include "util.php";
-
-// Init DB connection
-try {
-	$pdo = @new PDO("mysql:host=$_db_host;dbname=$_db_name", $_db_user, $_db_pass);
-	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-	L("Unable to connect to DB server:", $e);
-	$pdo = null;
-}
+include "init.php";
 
 // Build page content
 if ($pdo == null) {
