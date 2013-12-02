4fb8a27477e9b9e9e58be8cfcaaa17f1c81388fb
Benoit Duffez <benoit@bicou.net>
2013-12-02 23:55:34 +0100

    cleaned table creation query + fixed table prefix hard coded into the query

--- db/Strings.php
+++ db/Strings.php
@@ -12,16 +12,16 @@ class StringsDbAdapter extends DbAdapter {
 	protected function onUpgrade($oldVersion, $newVersion) {
 		if ($oldVersion < 1) {
 			$statement = <<<SQL
-CREATE TABLE IF NOT EXISTS `oat_strings` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `lang` varchar(8) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `text` text NOT NULL,
-  `formatted` tinyint(1) NOT NULL,
-  `date_created` date NOT NULL,
-  `date_updated` date NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `lang` (`lang`,`name`)
+CREATE TABLE IF NOT EXISTS table (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  lang varchar(8) NOT NULL,
+  name varchar(255) NOT NULL,
+  text text NOT NULL,
+  formatted tinyint(1) NOT NULL,
+  date_created date NOT NULL,
+  date_updated date NOT NULL,
+  PRIMARY KEY (id),
+  UNIQUE KEY lang (lang,name)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
 SQL;
 
