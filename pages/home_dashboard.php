91642366393f79b341e6c9d6f5696784e0fd199e
BenoitDuffez <benoit.duffez@gmail.com>
2014-03-13 14:26:09 +0100

    cleaned the dashboard

--- pages/home_dashboard.php
+++ pages/home_dashboard.php
@@ -9,15 +9,12 @@ $tr = new Translations();
 $cfg = new Config();
 
 $defaultLang = $cfg->getDefaultLanguage();
-$devLang = $defaultLang == null ? "none" : $languages[$defaultLang]; 
 
 $langs = $tr->getLangs();
 if ($defaultLang == null) {
 	echo '<p>You have an empty database. You need to <a href="%PATH%/import/">import an XML file</a> before you can translate anything.
 		Be sure to import the XML file in the language you are developing in!</p>';
 } else {
-	echo '<div>Development language: ' . $devLang . '</div>';
-
 	if (count($langs) == 0) {
 		echo '<p>You need to pick a now language to translate your strings to.
 		Please go to the <a href="%PATH%/translate/">translate tab</a> to pick one</p>';
