9aef8e37b45142d13ed7d18a6a600c5f1ee7cb23
BenoitDuffez <benoit.duffez@gmail.com>
2014-03-13 11:18:54 +0100

    improved app state when db is empty

--- pages/strings.php
+++ pages/strings.php
@@ -8,29 +8,38 @@ setHtmlTitle('Open Android Translator - Dashboard');
 $tr = new Translations();
 $cfg = new Config();
 
-echo '<div>Development language: ' . $languages[$cfg->getDefaultLanguage()] . '</div>
-<p>Available languages:</p>
-<ul>
-';
+$defaultLang = $cfg->getDefaultLanguage();
+$devLang = $defaultLang == null ? "none" : $languages[$defaultLang]; 
+
 
 $langs = $tr->getLangs();
-if (count($langs) == 0) {
-	echo "none";
+if ($defaultLang == null) {
+	echo '<p>You have an empty database. You need to <a href="%PATH%/import/">import an XML file</a> before you can translate anything.
+		Be sure to import the XML file in the language you are developing in!</p>';
 } else {
-	$max = $langs[$cfg->getDefaultLanguage()]['nb'];
-	foreach ($langs as $lang) {
-		echo '<li><a href="%PATH%/translate/' . $lang['lang'] . '/">';
-		echo $languages[$lang['lang']];
-		echo "</a>";
-		if ($lang['lang'] == $cfg->getDefaultLanguage()) {
-			echo " *";
+	echo '<div>Development language: ' . $devLang . '</div>';
+
+	if (count($langs) == 0) {
+		echo '<p>You need to pick a now language to translate your strings to.
+		Please go to the <a href="%PATH%/translate/">translate tab</a> to pick one</p>';
+	} else {
+		echo '<div>Available languages: ';
+		$max = $langs[$defaultLang]['nb'];
+		echo '<ul>';
+		foreach ($langs as $lang) {
+			echo '<li><a href="%PATH%/translate/' . $lang['lang'] . '/">';
+			echo $languages[$lang['lang']];
+			echo '</a>';
+			if ($lang['lang'] == $cfg->getDefaultLanguage()) {
+				echo ' *';
+			}
+			echo ' (' . $lang['nb'] . ' strings - ' . sprintf("%.1f %%", 100.0 * $lang['nb'] / $max) . ')</li>';
 		}
-		echo " (" . $lang['nb'] . " strings - " . sprintf("%.1f %%", 100.0 * $lang['nb'] / $max) . ")</li>";
+		echo '</ul></div>';
 	}
 }
 
 echo '
 </ul>
 ';
-
 
